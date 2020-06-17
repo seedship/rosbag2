38885a1c1f61ae5ad777032edd2c5683c305ef3b
Richard Nai <richardnai6@gmail.com>
2020-06-17 19:52:03 +0200

    Rearranging imports for issue #383

--- rosbag2_transport/src/rosbag2_transport/rosbag2_transport_python.cpp
+++ rosbag2_transport/src/rosbag2_transport/rosbag2_transport_python.cpp
@@ -12,14 +12,14 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#include <Python.h>
 #include <chrono>
 #include <memory>
 #include <string>
 #include <unordered_map>
 #include <utility>
 #include <vector>
+
+#include <Python.h>
 
 #include "rclcpp/qos.hpp"
 
